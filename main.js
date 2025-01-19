42adf6bfdf1f8df8139a041b4df7ee5d91aff310
tarax-desing <histor.iade0a100@gmail.com>
2025-01-19 21:10:26 +0100

    1

--- main.js
+++ main.js
@@ -4,35 +4,35 @@ const projects = [
     title: "<a href='https://www.artstation.com/axicblk' target='_blank' style='text-decoration: none; color: #00fff7; cursor: pointer; '>Artstation</a>",
     role: "Lead Game Designer",
     description: "I create immersive scenarios of innovative worlds",
-    // image: "img/artstation.png",
+    image: "img/artstation.png",
     tech: ["Unity", "C#", "Blender", "Photoshop"]
   },
   {
     title: "<a href='https://www.redbubble.com/es/people/axicdant3/shop?anchor=profile&asc=u' target='_blank' style='text-decoration: none; color: #00fff7; cursor: pointer; '>Store</a>",
     role: "unique designs",
     description: "Eclusive and personalized products.",
-    // image: "img/tienda.png",
+    image: "img/tienda.png",
     tech: ["Unreal Engine", "Blueprint", "Substance Painter"]
   },
   {
     title: "<a href='https://theh3llwalk3r.wixsite.com/danielariasperandres' target='_blank' style='text-decoration: none; color: #00fff7; cursor: pointer; '>Website</a>",
     role: "official website",
     description: "Here you can see all my works",
-    // image: "img/web.png",
+    image: "img/web.png",
     tech: ["Unity", "C#", "3DS Max", "Adobe XD"]
   },
   {
     title: "<a href='https://ko-fi.com/axicblk' target='_blank' style='text-decoration: none;color: #00fff7;cursor: pointer;'>Support me here</a>",
     role: "coffee",
     description: "Invite me for a coffee, if you liked my work",
-    // image: "img/taza-café.png",
+    image: "img/taza-café.png",
     tech: ["Unity", "C#", "3DS Max", "Adobe XD"]
   },
 {
   title: "<a href='https://www.amazon.es/dp/B0C6L43SDD?ref_=cm_sw_r_cp_ud_dp_T2P2F91HZZP84KQWS6F9' target='_blank' style='text-decoration: none; color: #00fff7; cursor: pointer; '>Amazon</a>",
   role: "Writer",
   description: "A science fiction book that will take you to unknown worlds.",
-  // image: "img/AMAZON.png",
+  image: "img/AMAZON.png",
   tech: ["World",]
 }
 ];
